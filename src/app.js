a484a87aa02256ad49e58659a41d87879bfdbfa1
ezequielpirrotta <54612652+ezequielpirrotta@users.noreply.github.com>
2023-02-07 19:01:24 -0300

    3ra entrega semanal

--- src/app.js
+++ src/app.js
@@ -11,11 +11,9 @@ app.use(Express.urlencoded({extended: true}));
 app.get('/products', async (request, response) => {
     let {limit} = request.query
     limit = parseInt(limit);
-    //console.log(typeof limit)
     let products = await pm.getProducts();
     if(limit !== undefined) {
         products = limit > 0? products.slice(0, limit) : [] 
-        //console.log(products)
         response.send(products);
     }
     else {
@@ -37,34 +35,3 @@ app.get('/products/:id', async (request, response) => {
 })
 
 app.listen(SERVER_PORT)
-
-
-
-
-/*
-for(let i = 9; i <= 12; i++){
-    let title = "producto prueba_"+i;
-    let description = "Este es un producto prueba";
-    let price = 20+i;
-    let thumbnail = "Sin imagen";
-    let code = "abc135"+i;
-    let stock = 25;
-    console.log("hola "+i)
-    await pm.addProduct(title, description, price, thumbnail, code, stock);
-}*/
-
-//console.log( await pm.getProducts());
-
-//console.log(await pm.getProductById(228))
-/*await pm.updateProduct({
-    id: 228,
-    field: "title",
-    newValue: "testeoo"
-})
-console.log("Productos actualizados")
-console.log(await pm.getProducts())
-/*
-pm.deleteProduct(521)
-console.log("Productos actualizados eliminación")
-console.log(pm.getProducts())
-*/
